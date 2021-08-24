4739eb08262d91e8d25174ddc1015746b363a939
GuerraJr <62675159+GuerraJr@users.noreply.github.com>
2021-08-23 21:51:54 -0300

    Add files via upload

---
+++ PhysicalConstants.py
@@ -0,0 +1,17 @@
+"""
+    c: light's velocity.
+    h: Planck's constant.
+    l: light's wavelength.
+    freq: light's frequency.
+    BRef:  reference bandwidth.
+    numPolarizations: is used to choose whether one or two polarizations are used to transmit the signal.
+        
+"""
+ 
+c = 299792458
+h = 6.62606957E-34
+l = 1550E-9
+freq = 193.4E12
+BRef = 12.5E9
+BSlot = 12.5E9    
+numPolarizations = 2
